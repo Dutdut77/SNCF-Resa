5f13458c035853eb953dde02c86b24ccbb7954e3
dutdut77 <dutdut77@hotmail.fr>
2024-07-02 18:32:43 +0200

    init signup

--- pages/signup.vue
+++ pages/signup.vue
@@ -10,6 +10,9 @@ useHead({
 });
 
 const formValue = ref({
+  secteur: "",
+  nom: "",
+  prenom: "",
   email: "",
   password: "",
 });
@@ -18,8 +21,8 @@ const validated = computed(() => {
   return formValue.value.email != "" && formValue.value.password != "" ? true : false;
 });
 
-const goToSignup = async () => {
-  await navigateTo("/signup");
+const goToLogin = async () => {
+  await navigateTo("/login");
 };
 </script>
 
@@ -27,13 +30,17 @@ const goToSignup = async () => {
   <section class="relative h-dvh w-full text-gray-50 flex flex-col bg-gradient-to-br from-slate-900 to-slate-700 pt-12 overflow-auto">
     <img class="absolute top-12 right-8 w-14" src="../assets/img/logo_sncf.png" alt="" />
 
-    <p class="font-bold text-2xl text-gray-100 px-8">Authentification</p>
+    <p class="font-bold text-2xl text-gray-100 px-8">Enregistrement</p>
     <div class="flex flex-col gap-4 pt-10 px-8">
-      <AppInput name="email" type="email" title="Email : " placeholder="Entrez votre email professionel" v-model="formValue.email" />
+      <AppInput name="secteur" type="text" title="Secteur : " placeholder="Entrez votre secteur" v-model="formValue.secteur" />
+      <AppInput name="nom" type="text" title="Nom : " placeholder="Entrez votre nom" v-model="formValue.nom" />
+      <AppInput name="prenom" type="text" title="Prénom : " placeholder="Entrez votre prénom" v-model="formValue.prenom" />
+      <AppInput name="email" type="email" title="Email : " placeholder="Entrez votre email pro" v-model="formValue.email" />
       <div class="flex flex-col">
         <AppInput name="password" type="password" title="Mot de passe : " placeholder="*****" v-model="formValue.password" />
-        <p class="text-xs ml-auto pt-2.5 text-gray-300">Mot de passe oublié ?</p>
+        <p class="text-xs ml-auto pt-2.5 text-gray-300">Minimun 8 charactères</p>
       </div>
+
       <AppButtonCarre class="ml-auto my-4" direction="" :validated="validated"> <template #default> Se connecter </template> </AppButtonCarre>
     </div>
     <div class="relative mt-auto">
@@ -47,7 +54,7 @@ const goToSignup = async () => {
         <path fill="#0284c7" fill-opacity="1" d="M0,128L48,138.7C96,149,192,171,288,154.7C384,139,480,85,576,80C672,75,768,117,864,117.3C960,117,1056,75,1152,85.3C1248,96,1344,160,1392,192L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
       </svg>
       <div class="h-16 w-full bg-[#0284c7] px-4"></div>
-      <p class="absolute bottom-0 left-0 right-0 text-center text-gray-50 text-sm p-4 cursor-pointer" @click="goToSignup()">Pas de compte ? Faire une demande</p>
+      <p class="absolute bottom-0 left-0 right-0 text-center text-gray-50 text-sm p-4 cursor-pointer" @click="goToLogin()">Déjà un compte ? Par ici</p>
     </div>
   </section>
 </template>
