4695185b8339653d38126ea0febe4b2452d836e0
dutdut77 <dutdut77@hotmail.fr>
2024-07-06 22:38:45 +0200

    init manifest

--- components/navbar.vue
+++ components/navbar.vue
@@ -10,13 +10,13 @@ import Add from "@/assets/svg/Add.vue";
   <section class="w-full h-20 bg-gray-700">
     <div class="h-14 flex justify-around items-center text-gray-400 border-t border-gray-600 px-4">
       <NuxtLink to="/home" class="relative group flex flex-col items-center flex-1 h-full justify-center">
-        <div class="h-[3px] bg-sky-500 absolute -top-0.5 left-0 right-0 group-hover:w-full duration-500 rounded-full" :class="'/home' === $route.path ? 'w-full' : 'w-0'"></div>
+        <div class="h-[3px] bg-sky-500 absolute -top-0.5 left-0 right-0 group-hover:w-full group-hover:duration-500 duration-200 rounded-full" :class="'/home' === $route.path ? 'w-full' : 'w-0'"></div>
         <Home class="h-6 w-6 group-hover:text-gray-200 duration-200" />
         <p class="text-xs pt-0.5 group-hover:text-gray-200 duration-200">Accueil</p>
       </NuxtLink>
 
       <NuxtLink to="/calendar" class="relative group flex flex-col items-center flex-1 h-full justify-center">
-        <div class="h-[3px] bg-sky-500 absolute -top-0.5 left-0 right-0 group-hover:w-full duration-500 rounded-full" :class="'/calendar' === $route.path ? 'w-full' : 'w-0'"></div>
+        <div class="h-[3px] bg-sky-500 absolute -top-0.5 left-0 right-0 group-hover:w-full group-hover:duration-500 duration-200 rounded-full" :class="'/calendar' === $route.path ? 'w-full' : 'w-0'"></div>
         <Calendar class="h-6 w-6 group-hover:text-gray-200 duration-200" />
         <p class="text-xs pt-0.5 group-hover:text-gray-200 duration-200">Calendrier</p>
       </NuxtLink>
@@ -26,13 +26,13 @@ import Add from "@/assets/svg/Add.vue";
       </NuxtLink>
 
       <NuxtLink to="/profil" class="relative group flex flex-col items-center flex-1 h-full justify-center">
-        <div class="h-[3px] bg-sky-500 absolute -top-0.5 left-0 right-0 group-hover:w-full duration-500 rounded-full" :class="'/profil' === $route.path ? 'w-full' : 'w-0'"></div>
+        <div class="h-[3px] bg-sky-500 absolute -top-0.5 left-0 right-0 group-hover:w-full group-hover:duration-500 duration-200 rounded-full" :class="'/profil' === $route.path ? 'w-full' : 'w-0'"></div>
         <User class="h-6 w-6 group-hover:text-gray-200 duration-200" />
         <p class="text-xs pt-0.5 group-hover:text-gray-200 duration-200">Profil</p>
       </NuxtLink>
 
       <NuxtLink to="/anomalies" class="relative group flex flex-col items-center flex-1 h-full justify-center">
-        <div class="h-[3px] bg-sky-500 absolute -top-0.5 left-0 right-0 group-hover:w-full duration-500 rounded-full" :class="'/anomalies' === $route.path ? 'w-full' : 'w-0'"></div>
+        <div class="h-[3px] bg-sky-500 absolute -top-0.5 left-0 right-0 group-hover:w-full group-hover:duration-500 duration-200 rounded-full" :class="'/anomalies' === $route.path ? 'w-full' : 'w-0'"></div>
         <Bandage class="h-6 w-6 group-hover:text-gray-200 duration-200" />
         <p class="text-xs pt-0.5 group-hover:text-gray-200 duration-200">Anomalies</p>
       </NuxtLink>
