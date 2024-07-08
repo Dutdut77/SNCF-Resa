96ff1c2dd19deb5ad961852e9595f236833fb6a1
dutdut77 <dutdut77@hotmail.fr>
2024-07-08 16:34:12 +0200

    pt-8 vehicules

--- components/resa/type.vue
+++ components/resa/type.vue
@@ -41,13 +41,13 @@ const formRadio = computed({
       </label>
     </div>
 
-    <div class="absolute top-1/2 h-16 w-full bg-slate-200 z-20 -mt-8 -skew-y-6 md:-skew-y-6 flex items-center justify-center text-slate-800 italic text-lg uppercase"><Arrow class="w-8 h-8 -rotate-90" /> Toucher pour réserver <Arrow class="w-8 h-8 rotate-90" /></div>
+    <div class="absolute top-1/2 h-16 w-full bg-slate-200 z-20 -mt-8 -skew-y-6 md:-skew-y-6 flex items-center justify-center text-slate-800 italic uppercase"><Arrow class="w-8 h-8 -rotate-90" /> Toucher pour réserver <Arrow class="w-8 h-8 rotate-90" /></div>
 
     <!-- VEHICULES -->
     <div class="relative w-full h-full">
       <input id="1" type="radio" v-model="formRadio" value="1" class="hidden peer" />
       <label for="1" class="h-full w-full flex items-center justify-center gap-2 bg-gradient-to-br from-transparent to-transparent border-slate-600 hover:bg-opacity-75 saturate-0 peer-checked:saturate-100 cursor-pointer transition duration-200">
-        <div class="w-1/2 h-full flex justify-center items-center">
+        <div class="w-1/2 h-full flex justify-center items-center pt-8">
           <div class="relative w-2/3 h-1/3 flex justify-center items-center">
             <span class="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-sky-700 to-sky-500 scale-110 rounded-[38%_62%_63%_37%_/_41%_44%_56%_59%]"></span>
             <div class="absolute top-0 left-0 w-full h-full border rounded-[38%_62%_63%_37%_/_41%_44%_56%_59%]"></div>
